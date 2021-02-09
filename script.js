0eb644bd19121aa3232f0dd619f3d226f5908100
nikhilakatiki <51728915+nikhilakatiki@users.noreply.github.com>
2021-02-08 19:17:20 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function TurnOn() {
+    document.getElementById("bulb").src = "pexels-photo-577514.jpeg";
+}
+
+function TurnOff() {
+    document.getElementById("bulb").src = "lightbulbg-header_resize_md.jpg";
+}
